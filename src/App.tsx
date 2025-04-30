4917b297629c428cd904d14e9f953a8be29d3ff1
Sam Colson <samcolson4@gmail.com>
2025-04-30 21:46:38 +0100

    Add basic page set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
 import Box from '@mui/material/Box';
 import HeaderBar from './components/header_bar'
+import { useState } from 'react';
 
 function App() {
+  const [page, setPage] = useState('home');
+
   return (
     <Box
       sx={{
@@ -12,10 +15,13 @@ function App() {
         width: '100%',
       }}
     >
-      <HeaderBar />
+      <HeaderBar setPage={setPage} />
       <Box sx={{ mt: 0.25 }}>
-        <hr style={{ width: '90%', margin: '0.25rem auto' }} />
-        <hr style={{ width: '90%', margin: '0.25rem auto' }} />
+        <hr style={{ width: '90%', margin: '0.1rem auto' }} />
+        <hr style={{ width: '90%', margin: '0.1rem auto' }} />
+
+        {page === 'rummy' && <div style={{ textAlign: 'center' }}>Rummy</div>}
+        {page === 'golf' && <div style={{ textAlign: 'center' }}>Golf</div>}
       </Box>
     </Box>
   )
--- src/components/header_bar.tsx
+++ src/components/header_bar.tsx
@@ -1,8 +1,24 @@
-function HeaderBar() {
+function HeaderBar({ setPage }: { setPage: (page: string) => void }) {
   return (
     <>
       <div>
         <h1>Goodridge Colson Games</h1>
+        <nav style={{ textAlign: "center", marginTop: "10px" }}>
+          <a
+            href="#"
+            onClick={() => setPage("rummy")}
+            style={{ margin: "0 15px" }}
+          >
+            Rummy
+          </a>
+          <a
+            href="#"
+            onClick={() => setPage("golf")}
+            style={{ margin: "0 15px" }}
+          >
+            Golf
+          </a>
+        </nav>
       </div>
     </>
   );
